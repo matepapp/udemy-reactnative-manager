4104504ad80419603f1cab7294df832654d8548a
Mate Papp <matepapp@icloud.com>
2017-07-11 13:57:43 +0200

    Lesson 105 - Boilerplate setup.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAviHOrPkkii3jFDdAv4eSLkLqHNPZPFIo',
+      authDomain: 'reactnative-manager-b7832.firebaseapp.com',
+      databaseURL: 'https://reactnative-manager-b7832.firebaseio.com',
+      projectId: 'reactnative-manager-b7832',
+      storageBucket: 'reactnative-manager-b7832.appspot.com',
+      messagingSenderId: '903374882554'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
