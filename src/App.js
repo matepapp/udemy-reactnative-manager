f943db877b562ae24cfbd2b86bb4a1516e01978e
Mate Papp <matepapp@icloud.com>
2017-07-11 18:00:35 +0200

    Lesson 116 - Redux thunk wire up.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // @flow
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { View, Text } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
@@ -22,8 +22,10 @@ export default class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
